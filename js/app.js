334abc817c36f951d0c5576932515ae49c5b4738
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-11-30 13:05:19 +0400

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
+
+
 const quizCategory = document.querySelector('.quiz-category'),
         startBtn = document.getElementById('start-game-btn'),
         quizDiff = document.querySelector('.quiz-diff'),
         gameContent = document.querySelector('.game-content'),
-        startFrom = document.querySelector('.start-form');
+        startFrom = document.querySelector('.start-form'),
+        nextQuestBtn = document.querySelector('.next-quest'),
+        questionCont = document.querySelector('.question-main')
+        question = document.querySelector('.question');
+
+const MAX_QUESTION = 10;
+let questionIndex = 0;
+let difficulty,
+    category;
 
 
+
+function getRandomQuest(Arr){
+    let length = Arr.length,
+    randomQuest = [],
+    i = 0;
+
+    while(length --){
+        i = Math.floor(Math.random() * (length +1 ));
+        randomQuest.push(Arr[i]);
+        Arr.splice(i, 1);
+    }
+    return randomQuest;
+    
+}
+
+
+function makeAnswers (Arr, correct) {
+    let content = '';
+    Arr.forEach((value, index)=> {
+        content += `<div class="ques-cont">
+            <p class="q-w">${index + 1}. </p>
+            <p class="answer">${value}</p>
+        </div>`;
+    });
+    content += '<p class="next-quest">Next question</p>';
+    questionCont.innerHTML = content;
+}
 
 
 function getCategories() {
@@ -16,16 +53,37 @@ function getCategories() {
         for(let i in response) {
             options += `<option class='categories'value=${response[i].id}>${response[i].name}</option>`;
         }
-        quizCategory.innerHTML = options;
     });
 }
 getCategories();
 
-
-
-
-function generateQustion() {
+function startQuizFunc() {
     startFrom.style.display = 'none';
     gameContent.style.display = 'flex';
+    category = quizCategory.value;
+    difficulty = quizDiff.value;
+    generateRandomAnwsers()
 }
-startFrom.addEventListener('click', generateQustion);
+
+
+
+function generateRandomAnwsers(){
+    fetch(`https://opentdb.com/api.php?amount=1&category=${category}&difficulty=${difficulty}&type=multiple`)
+    .then(response => response.json())
+    .then(data =>{
+        const response = data.results[0];
+        let questionArr = [];
+        let correctAnswer = response.correct_answer
+
+        for(let i in response.incorrect_answers){
+            questionArr.push(response.incorrect_answers[i]);
+        }
+        questionArr.push(correctAnswer);
+
+        let randomQuestionArr = getRandomQuest(questionArr);
+
+        makeAnswers(randomQuestionArr, correctAnswer);
+    });
+}
+
+startBtn.addEventListener('click', startQuizFunc);
